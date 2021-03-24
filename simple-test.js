552d2f915f10dce5226880fd914e49c8057071d9
Jonas Lieber <jns.lieber@gmail.com>
2021-03-24 06:58:44 +0100

    fixed simple test

--- simple-test.js
+++ simple-test.js
@@ -1,5 +1,10 @@
-const { sync } = require('./index')
+const { parse } = require('./index')
+const path = require('path')
 
-console.assert(sync(0) === 100, 'Simple test failed')
+const csvPath = path.join(__dirname, '__test__/fixtures/basic.csv')
+
+console.info('testing basic.csv parsing')
+
+console.assert(parse(csvPath).length == 1, 'Simple test failed')
 
 console.info('Simple test passed')
